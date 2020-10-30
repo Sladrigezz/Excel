42dc893f8407fafff23634776d3176cf81c43b31
Sladrigezz <Sladrigezz@github.com>
2020-10-30 22:30:41 +0300

    Store create

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
 import './scss/index.scss'
+import { createStore } from './core/createStore'
+import { rootReducer } from './redux/rootReducer'
+
+const ctore = createStore(rootReducer)
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table], store
 })
 
 excel.render()
