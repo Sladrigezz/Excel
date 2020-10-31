906b72b890c702cb88dcdc74c2e6e55c1926bd15
Sladrigezz <Sladrigezz@github.com>
2020-10-31 18:36:40 +0300

    styles state save

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,11 +1,23 @@
+import { defaultStyles } from "../constants"
 import { storage } from "../core/utils"
 
 const defaultState = {
     rowState: {},
     colState: {},
     dataState: {},
+    stylesState: {},
     currentText: '',
+    currentStyles: defaultStyles
+
 
 }
 
-export const initialState = storage('excel-state') ? storage('excel-state') : defaultState
+const normasize = state => ({
+    ...state,
+    currentStyles: defaultStyles,
+    currentText: ''
+})
+
+export const initialState = storage('excel-state')
+    ? normasize(storage('excel-state'))
+    : defaultState
